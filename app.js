39b3c67fcd9250d5582381b8e73a7d26dd3a08fc
Gazdag Balázs <gazdag.balazs8.a@gmail.com>
2023-04-18 17:49:43 +0200

    JavaScript lescriptelve

--- app.js
+++ app.js
@@ -3,17 +3,37 @@ let minus = document.getElementById("minus");
 let plus = document.getElementById("plus"); 
 let reset = document.getElementById("reset"); 
 let counter = document.getElementById("count"); 
-let count = document.getElementById("count").value; 
+
+let count = 0;
+counter.innerText = count; 
 
 minus.addEventListener("click", function(){
-    count - 1; 
+    count -= 1;  
+    counter.innerText = count;
+    updateClass();
 })
 
 plus.addEventListener("click", function(){
-    count + 1; 
+    count += 1;
+    counter.innerText = count; 
+    updateClass();
 })
 
 reset.addEventListener("click", function(){
     count = 0; 
+    counter.innerText = count; 
+    updateClass();
 })
 
+
+
+function updateClass(){
+    if (count > 0){
+        document.getElementById("count").classList.add("positive")
+    } else if (count < 0){
+        document.getElementById("count").classList.add("negative")
+    } else if (count === 0 ){
+        document.getElementById("count").classList.remove("positive")
+        document.getElementById("count").classList.remove("negative")
+    }
+}
